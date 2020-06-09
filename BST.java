04279839bd9353be78f133b874146067980fd5a5
MrZLeo <mrzleo@foxmail.com>
2020-06-09 16:08:28 +0800

    add more funtions in BST

--- BST.java
+++ BST.java
@@ -24,17 +24,20 @@ public class BST<E extends Comparable<E>> {
         return size == 0;
     }
 
+    // Recursion add.
     private Node add(Node root, E e){
         
         if (root == null) {
+            size++;
             root = new Node(e);
-        } else if (root.e.compareTo(e) > 0) {
+        }
+        
+        if (e.compareTo(root.e) > 0) {
             root.right = add(root, e);
-        } else if (root.e.compareTo(e) < 0) {
+        } else if (e.compareTo(root.e) < 0) {
             root.left = add(root, e);
         }
         
-        size++;
         return root;
     }
 
@@ -42,4 +45,92 @@ public class BST<E extends Comparable<E>> {
         add(this.root, e);
     } 
 
+    public boolean contains(E e){
+        return contains(root, e);
+    }
+
+    // Recursion contains
+    private boolean contains(Node node, E e){
+        if (node == null) {
+            return false;
+        }
+
+        if (node.e.compareTo(e) == 0) {
+            return true;
+        } else if (e.compareTo(node.e) < 0){
+            return contains(node.left, e);
+        } else {
+            return contains(node.right, e);
+        }
+    }
+
+    public void preorder(){
+        preorder(root);
+    }
+    // recursion preorder
+    private void preorder(Node node){
+        if (node == null) {
+            return;
+        }
+
+        System.out.println(node.e);
+        preorder(node.left);
+        preorder(node.right);
+    }
+
+    // sorted!
+    public void inorder(){
+        inorder(root);
+    }
+
+    private void inorder(Node node){
+        if (node == null) {
+            return;
+        }
+
+        inorder(node.left);
+        System.out.println(node.e);
+        inorder(node.right);
+    }
+
+    public void postorder(){
+        postorder(root);
+    }
+
+    private void postorder(Node node){
+        if (node == null) {
+            return;
+        }
+
+        postorder(node.left);
+        postorder(node.right);
+        System.out.println(node.e);
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        generateBSTString(root, 0, res);
+        return res.toString();        
+    }
+
+    private void generateBSTString(Node node, int depth, StringBuilder res){
+        if (node == null) {
+            res.append(generateBSTDepthString(depth) + "null\n");
+            return;
+        }
+
+        res.append(generateBSTDepthString(depth) + node.e + "\n");
+        generateBSTString(node.left, depth + 1, res);
+        generateBSTString(node.right, depth + 1, res);
+    }
+
+    private String generateBSTDepthString(int depth){
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < depth; i++) {
+            res.append("--");
+        }
+
+        return res.toString();
+    }
 }
