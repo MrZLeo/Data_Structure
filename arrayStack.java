afd0b77a216f19929fe2f02ea7de5fb3a8177200
MrZLeo <mrzleo@foxmail.com>
2020-06-21 22:00:53 +0800

    update old data strcture finished long time ago

---
+++ arrayStack.java
@@ -0,0 +1,53 @@
+public class arrayStack<E> implements Stack<E>{
+
+    Array<E> array;
+
+    public arrayStack(int capacity){
+        array = new Array<>(capacity);
+    }
+
+    public arrayStack(){
+        array = new Array<>();
+    }
+
+    @Override
+    public int getSize() {
+        return array.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return array.isEmpty();
+    }
+
+    @Override
+    public void push(E e) {
+        array.addLast(e);
+    }
+
+    @Override
+    public E pop() {
+        return array.removeLast();
+    }
+
+    @Override
+    public E peek() {
+        return array.getLast();
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        res.append("Stack: ");
+        res.append('[');
+        for (int i = 0; i < array.getSize(); i++) {
+            res.append(array.get(i));
+            if (i != array.getSize()-1) {
+                res.append(", ");
+            }
+        }
+        res.append("] <-- top");
+
+        return res.toString();
+    }
+}
