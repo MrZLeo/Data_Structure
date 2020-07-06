906163450052eb26abfd653ae58df48903a6371f
MrZLeo <mrzleo@foxmail.com>
2020-07-06 14:51:57 +0800

    add order and contains methods into AVLTree

--- code/Java/AVLTree.java
+++ code/Java/AVLTree.java
@@ -1,5 +1,9 @@
 package code.Java;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
+
 /**
  *
  * @author MrZLeo
@@ -179,4 +183,101 @@ public class AVLTree<K extends Comparable<K>, V> {
         add(root, key, value);
     }
 
+    // Recursion contains
+    private boolean contains(Node node, K key) {
+        if (node == null) {
+            return false;
+        }
+
+        if (node.key.compareTo(key) == 0) {
+            return true;
+        } else if (key.compareTo(node.key) < 0) {
+            return contains(node.left, key);
+        } else {
+            return contains(node.right, key);
+        }
+    }
+
+    public void preorder() {
+        preorder(root);
+    }
+
+    // recursion preorder
+    private void preorder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        System.out.println(node.key);
+        preorder(node.left);
+        preorder(node.right);
+    }
+
+    // No recursion preorder
+    public void preorderNR() {
+        Deque<Node> stack = new ArrayDeque<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node cur = stack.pop();
+
+            System.out.println(cur.key);
+            assert root != null;
+            if (root.right != null) {
+                stack.push(root.right);
+            }
+            if (root.left != null) {
+                stack.push(root.left);
+            }
+        }
+    }
+
+    public void levelorder() {
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            Node cur = queue.remove();
+            System.out.println(cur.key);
+
+            if (cur.left != null) {
+                queue.add(cur.left);
+            }
+            if (cur.right != null) {
+                queue.add(cur.right);
+            }
+        }
+    }
+
+    // sorted!
+    public void inorder() {
+        inorder(root);
+    }
+
+    private void inorder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        inorder(node.left);
+        System.out.println(node.key);
+        inorder(node.right);
+    }
+
+    public void postorder() {
+        postorder(root);
+    }
+
+    private void postorder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        postorder(node.left);
+        postorder(node.right);
+        System.out.println(node.key);
+    }
+
+    public void remove(K key) {
+
+    }
 }
